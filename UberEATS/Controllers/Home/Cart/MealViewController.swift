a8ad136705e00ba6f2d862337cebc30e1023d75b
sean7218 <sean7218@gmail.com>
2018-08-04 22:21:10 -0400

    finished the view layout

--- UberEATS/Controllers/Home/Cart/MealViewController.swift
+++ UberEATS/Controllers/Home/Cart/MealViewController.swift
@@ -11,25 +11,14 @@ import UIKit
 class MealItemViewController: UIViewController {
     
     
-    var titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "The Classic Quesadilla"
-        return label
-    }()
-    
-    var detailLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "598-848 Cal. Jack cheese and your choice of chicken, steak, pork carnitas, taco beef, fish or farm-fresh veggies, and beans. served with sour cream and salsa. 133 Cal."
-        label.numberOfLines = 4
-        return label
-    }()
+
     
     lazy var tableView: UITableView = {
         let tb = UITableView(frame: CGRect.zero)
         tb.translatesAutoresizingMaskIntoConstraints = false
         tb.register(MealItemTableViewCell.self, forCellReuseIdentifier: "MealItemCell")
+        tb.register(MealItemTitleCell.self, forCellReuseIdentifier: "MealItemTitleCell")
+        tb.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCellEmpty")
         tb.delegate = self
         tb.dataSource = self
         return tb
@@ -46,13 +35,15 @@ class MealItemViewController: UIViewController {
     var shareButton: UIButton = {
         let button = UIButton(type: UIButtonType.custom)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setImage(#imageLiteral(resourceName: "button-normal"), for: .normal)
+        button.setImage(#imageLiteral(resourceName: "button-pressed"), for: UIControlState.normal)
+        button.addTarget(self, action: #selector(handleShare), for: UIControlEvents.touchUpInside)
         return button
     }()
     
     var quantityButton: UIButton = {
         let button = UIButton(type: UIButtonType.custom)
         button.translatesAutoresizingMaskIntoConstraints = false
+        
         return button
     }()
     
@@ -66,66 +57,216 @@ class MealItemViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @objc func handleShare(){
+        print("Sharing button pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
     
     func setupViews() {
-        view.addSubview(cancelButton)
-        cancelButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
-                          right: nil, bottom: nil,
-                          paddingTop: 10, paddingLeft: 10, paddingRight: 0, paddingBottom: 0,
-                          width: 50, height: 50)
-        
-        view.addSubview(shareButton)
-        shareButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.rightAnchor,
-                            right: nil, bottom: nil,
-                            paddingTop: 10, paddingLeft: 0, paddingRight: 10, paddingBottom: 0,
-                            width: 50, height: 50)
-        
-        view.addSubview(titleLabel)
-        titleLabel.anchor(top: cancelButton.bottomAnchor, left: view.leftAnchor,
-                          right: view.rightAnchor, bottom: nil,
-                          paddingTop: 10, paddingLeft: 0, paddingRight: 0, paddingBottom: 0,
-                          width: 0, height: 100)
-        view.addSubview(detailLabel)
-        detailLabel.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor,
-                           right: view.rightAnchor, bottom: nil,
-                           paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0,
-                           width: 0, height: 100)
+
         view.addSubview(tableView)
-        tableView.anchor(top: detailLabel.bottomAnchor, left: view.leftAnchor,
-                         right: view.rightAnchor, bottom: view.bottomAnchor,
-                         paddingTop: 10, paddingLeft: 0, paddingRight: 0, paddingBottom: 20,
-                         width: 0, height: 0)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            ])
+        view.addSubview(cancelButton)
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            cancelButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            cancelButton.widthAnchor.constraint(equalToConstant: 50),
+            cancelButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        view.addSubview(shareButton)
+        NSLayoutConstraint.activate([
+            shareButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            shareButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            shareButton.widthAnchor.constraint(equalToConstant: 50),
+            shareButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
 }
 
 extension MealItemViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return 10
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        if (section == 0) {
+            return 1
+        } else {
+            return 4
+        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MealItemCell", for: indexPath) as! MealItemTableViewCell
-        return cell
+        if (indexPath.section == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MealItemTitleCell", for: indexPath) as! MealItemTitleCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MealItemCell", for: indexPath) as! MealItemTableViewCell
+            return cell
+        }
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
+        if (indexPath.section == 0){
+            return 300
+        } else {
+            return 50
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 0) {
+            return nil
+        } else {
+            return "someHeaderName"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if (section == 0) {
+            return 0
+        }
+        return 20
     }
     
 }
 
 class MealItemTableViewCell: UITableViewCell {
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Crab Meat"
+        return label
+    }()
+    
+    var priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "+1.03"
+        return label
+    }()
+    
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .red
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(){
+        addSubview(nameLabel)
+        NSLayoutConstraint.activate([
+            nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0),
+            nameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            nameLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -80),
+            nameLabel.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        addSubview(priceLabel)
+        NSLayoutConstraint.activate([
+            priceLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0),
+            priceLabel.leftAnchor.constraint(equalTo: nameLabel.rightAnchor, constant: 10),
+            priceLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
+            priceLabel.heightAnchor.constraint(equalToConstant: 50)
+            ])
+    }
+}
+
+class MealItemTableHeaderView: UITableViewHeaderFooterView {
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
+
+class MealItemTitleCell: UITableViewCell {
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "The Classic Quesadilla"
+        label.textColor = UIColor.black
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    var seperator: UIView = {
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.backgroundColor = UIColor.black
+        return line
+    }()
+    
+    var detailLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "598-848 Cal. Jack cheese and your choice of chicken, steak, pork carnitas, taco beef, fish or farm-fresh veggies, and beans. served with sour cream and salsa. 133 Cal."
+        label.numberOfLines = 4
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 12)
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(){
+        addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            titleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
+            titleLabel.heightAnchor.constraint(equalToConstant: 100)
+            ])
+        addSubview(seperator)
+        NSLayoutConstraint.activate([
+            seperator.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            seperator.widthAnchor.constraint(equalToConstant: 150),
+            seperator.heightAnchor.constraint(equalToConstant: 1)
+            ])
+        addSubview(detailLabel)
+        NSLayoutConstraint.activate([
+            detailLabel.topAnchor.constraint(equalTo: seperator.bottomAnchor, constant: 10),
+            detailLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            detailLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
+            detailLabel.heightAnchor.constraint(equalToConstant: 100)
+            ])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
